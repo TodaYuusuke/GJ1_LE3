2b45b996393fda0388ab2fe1929779d711522efa
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-09-04 13:30:29 +0900

    実行確認完了

---
+++ Class/Scene/Title.cpp
@@ -0,0 +1,16 @@
+#include "Title.h"
+
+using namespace LWP;
+using namespace LWP::Input;
+using namespace LWP::Primitive;
+using namespace LWP::Math;
+using namespace LWP::Utility;
+using namespace LWP::Object;
+
+// 初期化
+void Title::Initialize() {
+}
+
+// 更新
+void Title::Update() {
+}
