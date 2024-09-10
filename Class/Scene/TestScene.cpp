3c1725af3e1e153109a50568e653f9f83e3e1e59
Taka <k022g0068@g.neec.ac.jp>
2024-09-10 10:10:47 +0900

    いったんスキルツリー削除

--- Class/Scene/TestScene.cpp
+++ Class/Scene/TestScene.cpp
@@ -6,21 +6,21 @@ using namespace LWP::Math;
 // 初期化
 void TestScene::Initialize() {
 	player_.Initialize();
-	skilltree_.Initialize();
+	//skilltree_.Initialize();
 	mainCamera.transform.translation.z = -22;
 	mainCamera.transform.translation.y = 10;
 
 	enemyManager_.Initialize(&player_);
 
 	followCamera_.Initialize(&mainCamera);
-	//stage_.LoadShortPath("Stage/Stage.gltf");
-	//stage_.worldTF.rotation = Quaternion::CreateFromAxisAngle(Vector3::UnitY(), 1.57f);
+	stage_.LoadShortPath("Stage/Stage.gltf");
+	stage_.worldTF.rotation = Quaternion::CreateFromAxisAngle(Vector3::UnitY(), 1.57f);
 }
 
 // 更新
 void TestScene::Update() {
 	player_.Update();
-	skilltree_.Update();
+	//skilltree_.Update();
 	followCamera_.Update(player_.GetWorldPosition());
 	enemyManager_.Update();
 }
