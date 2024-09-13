0ccfae15857c2bd2de8a55d48e004dd332273a7d
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-09-13 14:03:36 +0900

    ウェーブ終了処理を修正

---
+++ Class/Enemy/Child/Decoy.cpp
@@ -0,0 +1,91 @@
+#include "Spider.h"
+
+using namespace LWP;
+using namespace LWP::Object;
+
+void Spider::ChildInit() {
+	model_.LoadShortPath("Enemy/SpiderEnemy/SpiderEnemy.gltf");
+	animation_.LoadFullPath("resources/model/Enemy/SpiderEnemy/SpiderEnemy.gltf", &model_);
+	animation_.name = "Spider";
+	// コライダー設定
+	collider_.worldTF.translation.y = 1.0f;
+	collider_.name = "Spider";
+	Collider::AABB& aabb = collider_.SetBroadShape(Collider::AABB());
+	aabb.min.y = -0.17f;
+	aabb.max.y = 0.7f;
+
+	audioWalk_.Load(audioPath_ + walkPath_);
+	audioDead_.Load(audioPath_ + deadPath_);
+
+}
+
+void Spider::ChildUpdate() {}
+
+void Spider::DebugGUI() {
+	// 状態
+	ImGui::Text(("State : " + behaviorStirng_[behavior_]).c_str());
+	if (ImGui::TreeNode("Model")) {
+		model_.DebugGUI();
+		ImGui::TreePop();
+	}
+	if (ImGui::TreeNode("Animation")) {
+		animation_.DebugGUI();
+		ImGui::TreePop();
+	}
+	if (ImGui::TreeNode("Collider")) {
+		collider_.DebugGUI();
+		ImGui::TreePop();
+	}
+	ImGui::Checkbox("isAlive:", &isAlive_);
+	ImGui::Text("----- State Change -----");
+	if (ImGui::Button("Normal")) { behaviorReq_ = Normal; }
+	if (ImGui::Button("Knockback")) { behaviorReq_ = Knockback; }
+	if (ImGui::Button("Dying")) { behaviorReq_ = Dying; }
+	if (ImGui::Button("DeadBody")) { behaviorReq_ = DeadBody; }
+}
+
+void Spider::SetVolume(float volume)
+{
+
+	volume_ = (audioVolume_ / volume);
+
+}
+
+void Spider::InitDeadBody()
+{
+	audioWalk_.Stop();
+	audioDead_.Play(volume_);
+}
+
+void Spider::InitNormal() {
+	// アニメーション再生
+	animation_.Play("01_Walk", true);
+}
+void Spider::UpdateNormal() {
+	float delta = Info::GetDefaultDeltaTimeF();
+
+	//プレイヤー方向への向きベクトルを計算
+	Math::Vector3 velo = (player_->GetWorldPosition() - model_.worldTF.GetWorldPosition()).Normalize();
+	velo.y = 0;
+	//移動速度をかけた分移動
+	model_.worldTF.translation += velo * (spd_ * delta);
+
+	//歩行サウンド処理
+	currentWalkSE_ += delta;
+	if (currentWalkSE_ > walkSEsec_) {
+		currentWalkSE_ -= walkSEsec_;
+
+		audioWalk_.Play(volume_);
+	}
+}
+
+void Spider::InitKnockback() {
+	IEnemy::InitKnockback();
+	// アニメーション再生
+	animation_.Play("00_Idle", true);
+}
+void Spider::InitDying() {
+	IEnemy::InitDying();
+	// アニメーション再生
+	animation_.Play("02_Die");
+}
--- Class/Enemy/EnemyManager.cpp
+++ Class/Enemy/EnemyManager.cpp
@@ -210,12 +210,7 @@ void EnemyManager::StartWave(int waveNum) {
 	}
 }
 bool EnemyManager::GetEndWave() {
-	int result = 0;
-	for (int i = 0; i < static_cast<int>(EnemyType::Count); i++) {
-		result += enemyProperty_[i].spawn;
-	}
-	result += enemies_.size();
-
+	int result = GetRemainingEnemy();	// 残敵確認
 	// 残敵0未満ならば終了
 	return result <= 0;
 }
