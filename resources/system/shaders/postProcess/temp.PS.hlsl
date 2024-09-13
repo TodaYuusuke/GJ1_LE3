4069f7096526d80a2ba74f34f85a9d01734b4c47
Taka <k022g0068@g.neec.ac.jp>
2024-09-13 16:43:17 +0900

    さぶも

--- resources/system/shaders/postProcess/temp.PS.hlsl
+++ resources/system/shaders/postProcess/temp.PS.hlsl
@@ -18,24 +18,11 @@ Texture2D<float32_t> gDepth : register(t1);
 SamplerState gSampler : register(s0);
 SamplerState gPointSampler : register(s1);
 
-struct VignettingData {
-	float32_t intensity;
-};
-ConstantBuffer<VignettingData> vData : register(b1);
-
-float32_t Vignetting(float32_t2 uv) {
-	float32_t2 correct = uv * (1.0f - uv.xy);
-	float vignette = correct.x * correct.y * 16.0f;
-	vignette = saturate(pow(vignette, vData.intensity));
-	return vignette;
-}
 float32_t4 main(PSInput input) : SV_TARGET {
 	float32_t4 output;
 	float2 uv = input.texcoord;
 
     output = gTexture.Sample(gSampler, uv);
-
-	output.rgb *= Vignetting(uv);
 
 	return output;
 }
