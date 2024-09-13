e517a33e44615b244d2922d9db805c7cc355627b
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2024-09-13 13:26:28 +0900

    一度コミット

--- Class/UI/GameUIManager.cpp
+++ Class/UI/GameUIManager.cpp
@@ -89,6 +89,27 @@ void GameUIManager::DebugGUI()
 			// 表示トリガー切り替え
 			ImGui::Checkbox("IsDisplay", &isDisplayObserver_.t);
 			
+			// 左スティック
+			if (ImGui::TreeNode("LeftStick")) {
+				leftStickUI_.DebugGUI();
+				ImGui::TreePop();
+			}
+			// 左スティック
+			if (ImGui::TreeNode("LeftStick")) {
+				buttonAUI_.DebugGUI();
+				ImGui::TreePop();
+			}
+			// 左スティック
+			if (ImGui::TreeNode("LeftStick")) {
+				buttonBUI_.DebugGUI();
+				ImGui::TreePop();
+			}
+			// 左スティック
+			if (ImGui::TreeNode("LeftStick")) {
+				triggerRUI_.DebugGUI();
+				ImGui::TreePop();
+			}
+
 			// HPゲージ系スプライト
 			if (ImGui::TreeNode("HPGauge")) {
 				// 最初のスプライトのImGuiを表示
@@ -278,6 +299,12 @@ void GameUIManager::SwitchDisplayUI(bool isDisplay)
 		enemySprite_.numFirst.n[i].isActive = false;
 		enemySprite_.numSecond.n[i].isActive = false;
 	}
+
+	// ボタン系UIの表示表示
+	leftStickUI_.isActive = isDisplay;
+	buttonAUI_.isActive = isDisplay;
+	buttoBUI_.isActive = isDisplay;
+	triggerRUI_.isActive = isDisplay;
 }
 
 void GameUIManager::WaveSprite::Init(LWP::Math::Vector3 pos) {
