4a04e07beed47b5edba8d854a6c3879a8f6c963a
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2024-09-13 19:01:52 +0900

    テキスト変更

--- resources/system/shaders/postProcess/temp.PS.hlsl
+++ resources/system/shaders/postProcess/temp.PS.hlsl
@@ -18,38 +18,10 @@ Texture2D<float32_t> gDepth : register(t1);
 SamplerState gSampler : register(s0);
 SamplerState gPointSampler : register(s1);
 
-struct RadialBlurData {
-	float32_t2 center;
-	float32_t blurWidth;
-};
-ConstantBuffer<RadialBlurData> radialData : register(b1);
-
-float32_t3 RadialBlur(float32_t3 rgb, float32_t2 uv) {
-	const int32_t kNumSamples = 10;
-	float32_t3 result = rgb;
-
-	float32_t2 direction = uv - radialData.center;
-	for(int32_t sampleIndex = 1; sampleIndex < kNumSamples; sampleIndex++) {
-		float32_t2 texcoord = uv + direction * radialData.blurWidth * float32_t(sampleIndex);
-		result += gTexture.Sample(gSampler, texcoord).rgb;
-	}
-
-	return result * rcp(kNumSamples);
-}
-struct GrayScaleData {
-	float32_t intensity;
-};
-ConstantBuffer<GrayScaleData> gsData : register(b2);
-
-float32_t3 GrayScale(float32_t3 color) {
-    float32_t3 result = dot(color, float32_t3(0.2125f, 0.7154f, 0.0721f));
-    return lerp(color, result, gsData.intensity);
-}
-
 struct VignettingData {
 	float32_t intensity;
 };
-ConstantBuffer<VignettingData> vData : register(b3);
+ConstantBuffer<VignettingData> vData : register(b1);
 
 float32_t Vignetting(float32_t2 uv) {
 	float32_t2 correct = uv * (1.0f - uv.xy);
@@ -62,10 +34,6 @@ float32_t4 main(PSInput input) : SV_TARGET {
 	float2 uv = input.texcoord;
 
     output = gTexture.Sample(gSampler, uv);
-
-	output.rgb = RadialBlur(output.rgb, uv);
-
-	output.rgb = GrayScale(output.rgb);
 
 	output.rgb *= Vignetting(uv);
 
